4e1d57f757987637b05e56eb75c76e92b8c551ab
gbeaud <gbeaud@microsoft.com>
2022-01-05 10:06:10 +0100

    Update config file name

--- avd-azure-image-builder/configurations.ps1
+++
@@ -1,3 +0,0 @@
-# This script is run upon image creation and may contain instructions and configurations to be applied to the image
-mkdir c:\buildArtifacts
-Write-Output Guillaumes script Was Here  > c:\buildArtifacts\azureImageBuilder.txt
