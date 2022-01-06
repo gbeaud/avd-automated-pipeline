a1a18d6707c1bdfd5b5bad2f7f3b3d7c843a89d7
gbeaud <gbeaud@microsoft.com>
2022-01-06 15:34:35 +0100

    Add config file

--- avd-azure-image-builder/ImageConfigurations.ps1
+++ avd-azure-image-builder/ImageConfigurations.ps1
@@ -1,3 +1,3 @@
 # This script is run upon image creation and may contain instructions and configurations to be applied to the image
 mkdir c:\buildArtifacts
-Write-Output Guillaumes script Was Here  > c:\buildArtifacts\azureImageBuilder.txt
+Write-Output "Guillaume's script was here"  > c:\buildArtifacts\azureImageBuilder.txt
