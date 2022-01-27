aec889c08a0ed1635764db14bbb3947d486fc503
gbeaud <gbeaud@microsoft.com>
2022-01-27 09:53:27 +0100

    Testing Terraform deployment from GitHub actions

--- avd-terraform-deployment/import.tf
+++ avd-terraform-deployment/import.tf
@@ -1,4 +1,7 @@
 # Importing the resource group into the state. If working locally, terraform stores the state in a .tfstate file. However if running from GitHub Actions or Azure DevOps, the state is not preserved between runs. Thus the resource group must be explicitely added in the state.
 
-
-resource "azurerm_resource_group" "rg-avd-terraform-githubactions-westeu-test-01" {}
+# Imports AVD Resource Group
+#resource "azurerm_resource_group" "rg" {
+#  name     = var.rg_name
+#  location = var.deploy_location
+#}
