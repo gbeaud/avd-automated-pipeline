e44b8cd097d20cbe9faa5f8f5cd10cfd93d9fc1d
gbeaud <gbeaud@microsoft.com>
2022-02-11 10:51:15 +0100

    Testing Terraform with GitHub Actions

--- avd-terraform-deployment/main.tf
+++ avd-terraform-deployment/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_virtual_desktop_host_pool" "hostpool" {
 # Creates a resource for the registration token
 resource "azurerm_virtual_desktop_host_pool_registration_info" "registration_info" {
   hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
-  expiration_date = "2025-01-01T23:40:52Z"
+  expiration_date = "2022-03-03T23:40:52Z"
 }
 
 # Create AVD DAG
