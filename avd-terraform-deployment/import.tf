1a5115dcbbfdfe45eec4a377a2fca4a067c93f65
gbeaud <79973892+gbeaud@users.noreply.github.com>
2022-02-23 15:17:50 +0100

    Delete import.tf

--- avd-terraform-deployment/import.tf
+++
@@ -1,7 +0,0 @@
-# Importing the resource group into the state. If working locally, terraform stores the state in a .tfstate file. However if running from GitHub Actions or Azure DevOps, the state is not preserved between runs. Thus the resource group must be explicitely added in the state.
-
-# Imports AVD Resource Group
-#resource "azurerm_resource_group" "rg" {
-#  name     = var.rg_name
-#  location = var.deploy_location
-#}
