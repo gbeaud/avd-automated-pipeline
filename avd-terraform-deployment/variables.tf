8355a8a02d98533cb7b7ae04a6706040f8b9fb9f
gbeaud <gbeaud@microsoft.com>
2022-03-10 14:59:25 +0100

    Fix formatting issue in TF files

--- avd-terraform-deployment/variables.tf
+++ avd-terraform-deployment/variables.tf
@@ -1,6 +1,6 @@
 variable "rg_name" {
   type        = string
-  default     = "rg-avd-terraform-githubactions-westeu-test-01"
+  # default     = "rg-avd-terraform-githubactions-westeu-test-01"
   description = "Name of the Resource group in which to deploy the AVD resources"
 }
 
