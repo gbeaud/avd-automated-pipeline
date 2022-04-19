96b87299099bb68feaab598fce15b5515222b505
gbeaud <gbeaud@microsoft.com>
2022-04-19 17:58:00 +0200

    Adding image version as variable + deleting previous version with same name

--- avd-azure-image-builder/ImageConfigurations.ps1
+++ avd-azure-image-builder/ImageConfigurations.ps1
@@ -1,3 +1,3 @@
 # This script is run upon image creation and may contain instructions and configurations to be applied to the image
 mkdir c:\buildArtifacts
-Write-Output "Guillaume's script was here, triggered from GitHub Actions on April 19th, 2022"  > c:\buildArtifacts\azureImageBuilder.txt
+Write-Output "Guillaume's script was here, triggered from GitHub Actions on April 19th, 2022 at 1757"  > c:\buildArtifacts\azureImageBuilder.txt
