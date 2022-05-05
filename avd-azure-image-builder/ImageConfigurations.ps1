677e262263c2b15d94d088e761eb59792e70b14c
gbeaud <gbeaud@microsoft.com>
2022-05-05 09:14:47 +0200

    Adding new custom image

--- avd-azure-image-builder/ImageConfigurations.ps1
+++ avd-azure-image-builder/ImageConfigurations.ps1
@@ -1,3 +1,3 @@
 # This script is run upon image creation and may contain instructions and configurations to be applied to the image
 mkdir c:\buildArtifacts
-Write-Output "Guillaume's script was here, triggered from GitHub Actions on May 4th"  > c:\buildArtifacts\azureImageBuilder.txt
+Write-Output "Guillaume's script was here, triggered from GitHub Actions on May 5th"  > c:\buildArtifacts\azureImageBuilder.txt
